d818e46eb7f95566229091812ab75639c8d5ae9c
georgiev84 <cska30>
2019-10-05 00:42:20 +0300

    Initial commit

---
+++ src/main/java/realestate/web/controllers/OfferController.java
@@ -0,0 +1,14 @@
+package realestate.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class OfferController {
+
+    @GetMapping("/reg")
+    public String register(){
+        return "register.html";
+    }
+
+}
